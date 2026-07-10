5d1380e64918e097e61c2a621b05a1a70fe63842
Tarunbk648 <tarunbk1947@gmail.com>
2026-07-10 11:02:32 +0530

    Implement input validation in BankingServiceImpl
    Added validation checks for user input in save, update, and delete methods.

--- src/main/java/com/xworkz/bankingApplication/service/impl/BankingServiceImpl.java
+++ src/main/java/com/xworkz/bankingApplication/service/impl/BankingServiceImpl.java
@@ -8,10 +8,16 @@ import com.xworkz.bankingApplication.service.BankingService;
 
 public class BankingServiceImpl implements BankingService {
     BankingDao bankingDao = new BankingDaoImpl();
+    BankingDto bankingDto = new BankingDto();
 
     @Override
     public boolean save(BankingDto dto) {
-        return bankingDao.save(dto);
+        if (dto.getUser_Id() > 0 && dto.getUser_Name() != null && dto.getUser_Name().length() > 0 && dto.getUser_Email() != null && dto.getUser_Email().contains("@") && dto.getUser_Email().contains(".") && dto.getUser_Password() != null && dto.getAddress() != null) {
+            return bankingDao.save(dto);
+        } else {
+            System.err.println("Please fill the correct entries!");
+        }
+        return false;
     }
 
     @Override
@@ -19,24 +25,46 @@ public class BankingServiceImpl implements BankingService {
         return bankingDao.getAllDetailsByEmail(email);
     }
 
+
     @Override
     public boolean updateUserNameByUserId(int userId, String userName) {
-        return bankingDao.updateUserNameByUserId(userId,userName);
+        if (userId != 0 && userId == bankingDto.getUser_Id()) {
+            return bankingDao.updateUserNameByUserId(userId, userName);
+        } else {
+            System.err.println("Please enter the correct userId!");
+        }
+        return false;
     }
 
     @Override
     public boolean updatePasswordByUserId(int userId, String password) {
-        return bankingDao.updateUserNameByUserId(userId,password);
+        if (userId != 0 && userId == bankingDto.getUser_Id()) {
+            return bankingDao.updateUserNameByUserId(userId, password);
+        } else {
+            System.err.println("Please enter the correct userId!");
+        }
+        return false;
     }
 
     @Override
     public boolean updateAddressByUserName(String address, String name) {
-        return bankingDao.updateAddressByUserName(address,name);
+        if (name != null && name.equalsIgnoreCase(bankingDto.getUser_Name())) {
+            return bankingDao.updateAddressByUserName(address, name);
+        } else {
+            System.err.println("Please enter the correct name!");
+        }
+        return false;
     }
 
     @Override
     public boolean deleteDataByUserId(int userId) {
+        if(userId!=0 ){
         return bankingDao.deleteDataByUserId(userId);
+    }
+        else{
+            System.err.println("Please enter the correct userId!");
+        }
+        return false;
     }
 
     @Override

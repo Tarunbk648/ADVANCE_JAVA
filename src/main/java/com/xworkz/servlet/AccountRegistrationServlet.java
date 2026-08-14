327cf2e50f7a193e0300656ed4d06610aafe8628
tarun <tarunbk1947@gmail.com>
2026-08-14 18:25:11 +0530

    Initial commit

---
+++ src/main/java/com/xworkz/servlet/AccountRegistrationServlet.java
@@ -0,0 +1,18 @@
+package com.xworkz.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/register")
+public class AccountRegistrationServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        PrintWriter printWriter= servletResponse.getWriter();
+        printWriter.print("Thank you! your account has been successfully created");
+    }
+}
